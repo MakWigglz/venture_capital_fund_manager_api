a7483916af1c31d87695c9df2901d3d7a3897c5d
MakWigglz <wigglz734@gmail.com>
2025-06-03 11:41:38 +0000

    extensions.py

--- extensions.py
+++ extensions.py
@@ -1,6 +1,10 @@
+# In venture_capital_fund_manager_api/extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager # If used
+
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
+jwt = JWTManager() # If used

587b816ead84ab0ff31ee27fb0d92004df61559d
MakWigglz <wigglz734@gmail.com>
2025-06-03 21:42:47 +0000

    indent fix

--- src/__init__.py
+++ src/__init__.py
@@ -23,7 +23,7 @@ def create_app():
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
-	jwt.init_app(app) # Initialize JWT
+    jwt.init_app(app)  # Initialize JWT
 
     # Register blueprints
     app.register_blueprint(api_bp, url_prefix='/api')
